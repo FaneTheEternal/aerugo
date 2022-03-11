be49438d712931fa5fb9c9fe2db9db895639f80e
Владимир <artemyev.vova@gmail.com>
2022-03-11 07:46:02 +0300

    иной вид текстовых шагов

--- src/bin/game/startup/spawn_game.rs
+++ src/bin/game/startup/spawn_game.rs
@@ -21,6 +21,7 @@ pub(crate) fn spawn_game(
     let mut text_flow_entity = ui_text;
     let mut text_narrator_entity = ui_text;
     let mut narrator_entity = ui_text;
+    let mut text_items = ui_text;
 
     let mut ui_phrase = Entity::from_raw(0);
 
@@ -44,117 +45,145 @@ pub(crate) fn spawn_game(
                         size: SIZE_ALL,
                         padding: Rect::all(Val::Px(10.0)),
                         position_type: PositionType::Absolute,
-                        flex_wrap: FlexWrap::Wrap,
-                        flex_direction: FlexDirection::Column,
                         ..Default::default()
                     },
                     color: TRANSPARENT.into(),
                     ..Default::default()
                 });
             ui_text = entity
-                // flow
                 .with_children(|parent| {
                     parent
-                        // wrapper
                         .spawn_bundle(NodeBundle {
                             style: Style {
-                                size: Size::new(Val::Percent(100.0), Val::Percent(20.0)),
-                                padding: Rect::all(Val::Px(10.0)),
+                                size: Size::new(Val::Percent(100.0), Val::Percent(30.0)),
+                                flex_wrap: FlexWrap::Wrap,
+                                flex_direction: FlexDirection::Row,
                                 ..Default::default()
                             },
                             color: TRANSPARENT.into(),
                             ..Default::default()
                         })
+                        // narrator
                         .with_children(|parent| {
-                            parent
-                                .spawn_bundle(NodeBundle {
+                            narrator_entity = parent
+                                .spawn_bundle(ImageBundle {
                                     style: Style {
-                                        size: SIZE_ALL,
-                                        align_items: AlignItems::FlexStart,
-                                        align_content: AlignContent::FlexStart,
-                                        flex_direction: FlexDirection::ColumnReverse,
-                                        flex_wrap: FlexWrap::Wrap,
+                                        size: Size::new(Val::Percent(20.0), Val::Percent(100.0)),
+                                        display: Display::None,
                                         ..Default::default()
                                     },
-                                    color: GLASS_RED.into(),
                                     ..Default::default()
                                 })
+                                .id();
+                        })
+                        // text items
+                        .with_children(|parent| {
+                            text_items = parent
+                                .spawn_bundle(NodeBundle {
+                                    style: Style {
+                                        flex_wrap: FlexWrap::Wrap,
+                                        flex_direction: FlexDirection::Column,
+                                        flex_grow: 1.0,
+                                        flex_shrink: 0.0,
+                                        ..Default::default()
+                                    },
+                                    color: TRANSPARENT.into(),
+                                    ..Default::default()
+                                })
+                                // flow
                                 .with_children(|parent| {
-                                    narrator_entity = parent
-                                        .spawn_bundle(ImageBundle {
+                                    parent
+                                        // wrapper
+                                        .spawn_bundle(NodeBundle {
                                             style: Style {
-                                                size: Size::new(Val::Percent(20.0), Val::Percent(100.0)),
-                                                display: Display::None,
+                                                size: Size::new(Val::Percent(100.0), Val::Percent(67.0)),
+                                                padding: Rect::all(Val::Px(10.0)),
                                                 ..Default::default()
                                             },
+                                            color: TRANSPARENT.into(),
                                             ..Default::default()
                                         })
-                                        .id();
+                                        .with_children(|parent| {
+                                            parent
+                                                .spawn_bundle(NodeBundle {
+                                                    style: Style {
+                                                        size: SIZE_ALL,
+                                                        align_items: AlignItems::FlexStart,
+                                                        align_content: AlignContent::FlexStart,
+                                                        flex_direction: FlexDirection::ColumnReverse,
+                                                        flex_wrap: FlexWrap::Wrap,
+                                                        ..Default::default()
+                                                    },
+                                                    color: GLASS_RED.into(),
+                                                    ..Default::default()
+                                                })
+                                                .with_children(|parent| {
+                                                    text_flow_entity = parent
+                                                        .spawn_bundle(TextBundle {
+                                                            text: Text::with_section(
+                                                                "Some text",
+                                                                TextStyle {
+                                                                    font: text_font.clone(),
+                                                                    font_size: 20.0,
+                                                                    color: Color::GREEN,
+                                                                },
+                                                                TextAlignment::default(),
+                                                            ),
+                                                            ..Default::default()
+                                                        })
+                                                        .id();
+                                                });
+                                        });
                                 })
+                                // narrator
                                 .with_children(|parent| {
-                                    text_flow_entity = parent
-                                        .spawn_bundle(TextBundle {
-                                            text: Text::with_section(
-                                                "Some text",
-                                                TextStyle {
-                                                    font: text_font.clone(),
-                                                    font_size: 20.0,
-                                                    color: Color::GREEN,
-                                                },
-                                                TextAlignment::default(),
-                                            ),
+                                    parent
+                                        // wrapper
+                                        .spawn_bundle(NodeBundle {
+                                            style: Style {
+                                                size: Size::new(Val::Percent(100.0), Val::Percent(33.0)),
+                                                padding: Rect::all(Val::Px(10.0)),
+                                                ..Default::default()
+                                            },
+                                            color: TRANSPARENT.into(),
                                             ..Default::default()
                                         })
-                                        .id();
-                                });
-                        });
-                })
-                // narrator
-                .with_children(|parent| {
-                    parent
-                        // wrapper
-                        .spawn_bundle(NodeBundle {
-                            style: Style {
-                                size: Size::new(Val::Percent(100.0), Val::Percent(10.0)),
-                                padding: Rect::all(Val::Px(10.0)),
-                                ..Default::default()
-                            },
-                            color: TRANSPARENT.into(),
-                            ..Default::default()
-                        })
-                        .with_children(|parent| {
-                            parent
-                                .spawn_bundle(NodeBundle {
-                                    style: Style {
-                                        size: SIZE_ALL,
-                                        align_items: AlignItems::FlexStart,
-                                        align_content: AlignContent::FlexStart,
-                                        flex_direction: FlexDirection::ColumnReverse,
-                                        flex_wrap: FlexWrap::Wrap,
-                                        ..Default::default()
-                                    },
-                                    color: GLASS_RED.into(),
-                                    ..Default::default()
+                                        .with_children(|parent| {
+                                            parent
+                                                .spawn_bundle(NodeBundle {
+                                                    style: Style {
+                                                        size: SIZE_ALL,
+                                                        justify_content: JustifyContent::Center,
+                                                        align_items: AlignItems::FlexStart,
+                                                        flex_direction: FlexDirection::ColumnReverse,
+                                                        flex_wrap: FlexWrap::Wrap,
+                                                        ..Default::default()
+                                                    },
+                                                    color: GLASS_RED.into(),
+                                                    ..Default::default()
+                                                })
+                                                .with_children(|parent| {
+                                                    text_narrator_entity = parent
+                                                        .spawn_bundle(TextBundle {
+                                                            text: Text::with_section(
+                                                                "Narrator",
+                                                                TextStyle {
+                                                                    font: text_font.clone(),
+                                                                    font_size: 20.0,
+                                                                    color: Color::GREEN,
+                                                                },
+                                                                TextAlignment {
+                                                                    vertical: VerticalAlign::Top,
+                                                                    horizontal: HorizontalAlign::Left,
+                                                                },
+                                                            ),
+                                                            ..Default::default()
+                                                        })
+                                                        .id();
+                                                });
+                                        });
                                 })
-                                .with_children(|parent| {
-                                    text_narrator_entity = parent
-                                        .spawn_bundle(TextBundle {
-                                            text: Text::with_section(
-                                                "Narrator",
-                                                TextStyle {
-                                                    font: text_font.clone(),
-                                                    font_size: 20.0,
-                                                    color: Color::GREEN,
-                                                },
-                                                TextAlignment {
-                                                    vertical: VerticalAlign::Top,
-                                                    horizontal: HorizontalAlign::Left,
-                                                },
-                                            ),
-                                            ..Default::default()
-                                        })
-                                        .id();
-                                });
+                                .id();
                         });
                 })
                 .id();
