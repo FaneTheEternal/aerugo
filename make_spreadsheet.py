a5e77a9bf8b1d069090a7e1adfffa562e8bd11c3
FaneTheEternal <artemyev.vova@gmail.com>
2022-09-13 19:09:43 +0300

    скрипт генерации таблицы анимаций

---
+++ make_spreadsheet.py
@@ -0,0 +1,30 @@
+import math
+import os
+import sys
+from pathlib import Path
+
+from PIL import Image
+
+dst = Path(sys.argv[1])
+frames = [Image.open(dst.joinpath(name)) for name in os.listdir(dst)]
+print(f'{len(frames)} frames')
+
+a = 1
+for i in range(1, int(math.sqrt(len(frames)) + 1)):
+    if len(frames) % i == 0:
+        a = i
+b = len(frames) // a
+a, b = max(a, b), min(a, b)
+print(f'{b} x {a}')
+assert b <= 8 and a <= 11, 'Too big spreadsheet'
+rectangle = [frames[b * i: b * (i + 1)] for i in range(a)]
+
+size = rectangle[0][0].size
+print(size)
+result = Image.new('RGBA', (size[0] * b, size[1] * a), (0, 0, 0, 0))
+
+for i, row in enumerate(rectangle):
+    for j, img in enumerate(row):
+        result.paste(img, (size[0] * j, size[1] * i))
+
+result.save('assets/spreadsheet.png')
